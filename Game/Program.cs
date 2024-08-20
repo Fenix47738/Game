b32f56aa6eac32419fab894e81511235f038522e
Fenix <vita96063@gmail.com>
2024-08-20 12:10:59 +0300

    Start

---
+++ Game/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Game
+{
+    public class Program
+    {
+        private static byte width = 60;
+        private static byte height = 24;
+        
+        public static void Main(string[] args)
+        {
+            GenerateMap();
+
+            Console.ReadKey();
+        }
+
+        private static void GenerateMap()
+        {
+            for (byte col = 0; col < height; col++)
+            {
+                for (byte row = 0; row < width; row++)
+                {
+                    if (row == 0 || col == 0 || row == width - 1 || col == height - 1)
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
